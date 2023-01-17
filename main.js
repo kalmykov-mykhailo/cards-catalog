d467d4382c4d6a02ce84c483226dfb8060206797
Kalmykov Mikhail <kalmykov.mikhail2@gmail.com>
2023-01-17 18:36:09 +0200

    task was completed

--- main.js
+++ main.js
@@ -2,35 +2,48 @@
 
 const rootElement = document.getElementById('root');
 const catalog = document.getElementById('catalog');
-const cardsTotalAmount = 32;
-const step = 3;
+const totalCardAmount = 36;
 const sizeImages = '1800/1800';
+const step = 3;
+let cardsToDisplay = totalCardAmount / 3;
+let initilIndex = 1;
 
 function createCardItem(id) {
   const element = document.createElement('li');
   element.classList.add('card');
   element.textContent = `Card Item ${id}`
-  
+
   return element;
+}
+
+function setSrc(imgEl, id) {
+  imgEl.src = `https://picsum.photos/id/${id}/${sizeImages}`;
+
+  imgEl.onerror = () => {
+    const newId = id + totalCardAmount * 3;
+    imgEl.id = newId;
+    setSrc(imgEl, newId);
+    createListeners(imgEl);
+  };
 }
 
 function createImage(id) {
   const element = document.createElement('img');
   element.classList.add('card__image');
   element.id = id;
-  element.dataset.src = `https://picsum.photos/id/${id}/${sizeImages}`;
   element.alt = `image-id-${id}`;
-  element.src = `https://picsum.photos/id/1/1/1`;
+  setSrc(element, id)
 
   return element;
 }
 
 function createListeners(element) {
-  const initialSrc = element.dataset.src;
+  const initialSrc = element.src;
+
   element.addEventListener('mouseenter', () => {
     const newId = +element.id + 1;
 
-    element.src = `https://picsum.photos/id/${newId}/${sizeImages}`;
+    setSrc(element, newId);
   })
 
   element.addEventListener('mouseleave', () => {
@@ -38,59 +51,32 @@ function createListeners(element) {
   })
 }
 
-for (let index = 1; index <= cardsTotalAmount * step; index += step) {
-  const liElement = createCardItem(index);
-  const staticImgElement = createImage(index);
-  const dynamicImgElemen = createImage(index + 2);
+function generateCards() {
+  for (let index = initilIndex; index <= cardsToDisplay * step; index += step) {
+    const liElement = createCardItem(index);
+    const staticImgElement = createImage(index);
+    const dynamicImgElemen = createImage(index + 2);
+  
+    createListeners(staticImgElement);
+  
+    liElement.appendChild(staticImgElement);
+    liElement.appendChild(dynamicImgElemen);
+    catalog.appendChild(liElement);
+  }
 
-  createListeners(staticImgElement);
-
-  liElement.appendChild(staticImgElement);
-  liElement.appendChild(dynamicImgElemen);
-  catalog.appendChild(liElement);
-}
-
-for (let index = 2; index < cardsTotalAmount * step; index += step) {
-  preloadImage(index)
-}
-
-function preloadImage(id) {
-  new Image().src = `https://picsum.photos/id/${id}/${sizeImages}`;
-}
-
-const lazyImages = document.querySelectorAll('img[data-src]');
-const windowHeigth = document.documentElement.clientHeight;
-
-let lazyImagesPosition = [];
-if (lazyImages.length > 0) {
-  lazyImages.forEach(img => {
-    if (img.dataset.src) {
-      lazyImagesPosition.push(img.getBoundingClientRect().top + pageYOffset);
-      lazyScrollCheck();
-    }
-  })
-}
-
-window.addEventListener('scroll', lazyScroll);
-
-function lazyScroll() {
-  if (document.querySelectorAll('img[data-src]').length > 0) {
-    lazyScrollCheck();
+  if (cardsToDisplay >= totalCardAmount) {
+    catalog.removeEventListener("scroll", scrollListener)
   }
 }
 
-function lazyScrollCheck() {
-  let imgIndex = lazyImagesPosition.findIndex(
-    item => pageYOffset > item - windowHeigth
-  );
-
-  if (imgIndex >= 0) {
-    if (lazyImages[imgIndex].dataset.src) {
-      lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
-      lazyImages[imgIndex].removeAttribute('data-src')
-    }
+function scrollListener() {
+  if (catalog.scrollTop >= (catalog.scrollHeight - catalog.clientHeight) * 0.8) {
+    initilIndex = cardsToDisplay * 3 + 1;
+    cardsToDisplay += cardsToDisplay;
+    generateCards();
   }
-
-  delete lazyImagesPosition[imgIndex];
 }
 
+catalog.addEventListener("scroll", scrollListener);
+
+generateCards()
